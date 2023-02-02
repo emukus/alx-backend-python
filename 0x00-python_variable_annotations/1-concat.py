e119cc347cef03d9752ea367b723ae0a6cb7f75d
emukus <mukuna.eugene@gmail.com>
2023-02-02 18:14:03 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""A type-annotated 'concat' function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Takes two strings as an argument and returns their sum"""
+    return str1 + str2
