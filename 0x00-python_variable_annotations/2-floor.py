8047636da32b4392230115772485b85b518f1ae6
emukus <mukuna.eugene@gmail.com>
2023-02-02 18:20:32 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A type-annotated 'floor' function"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Takes a float as an argument and returns the floor of the float"""
+    return math.floor(n)
