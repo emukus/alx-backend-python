37efebb59b21b6a92b623a1d12fb668f2344296e
emukus <mukuna.eugene@gmail.com>
2023-02-02 20:56:18 +0000

    More involved type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Given the parameters and return values, add type
+annotations to the function"""
+from typing import Mapping, Any, Union, TypeVar
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
+    """Returns value if it exists"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
