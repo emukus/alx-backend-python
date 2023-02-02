0155ab55d57660886ba4e717a41f2cccaad0d3c5
emukus <mukuna.eugene@gmail.com>
2023-02-02 18:04:32 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""A type-annotated 'add' function"""
+
+
+def add(a: float, b: float) -> float:
+    """Takes two floats as arguments and returns their sum as a float"""
+    return a + b
