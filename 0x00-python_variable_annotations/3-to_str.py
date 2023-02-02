69877ba92a4040f2f2620890e59e4c3c6ea7cfaf
emukus <mukuna.eugene@gmail.com>
2023-02-02 18:31:33 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""A type-annotated 'to_str' function"""
+
+
+def to_str(n: float) -> str:
+    """Takes a float as an argument and returns its string representation"""
+    return str(n)
