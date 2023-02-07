1fe01f180a2be37d2bea760c833d73c8d6f49273
emukus <mukuna.eugene@gmail.com>
2023-02-07 21:40:58 +0000

    Async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """A coroutine that takes no arguments."""
+
 from asyncio import sleep
 from random import uniform
 from typing import Generator
@@ -8,6 +9,6 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """Loops 10 times, each time asynchronously waits 1 sec,
     then yield a random int between 0 and 10."""
-    for i in range(10):
+    for _ in range(10):
         await sleep(1)
         yield uniform(0, 10)
