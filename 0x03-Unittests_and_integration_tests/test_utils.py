55fe09545155aeaac6a913d973fe04c426fc042f
emukus <mukuna.eugene@gmail.com>
2023-02-24 15:17:30 +0000

    0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        from utils import access_nested_map
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        from utils import access_nested_map
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), f"Key not found: {path[-1]}")
